93c6ba2da71b56ae60f4ad50e751379a82578403
PiiterLiao <pittpeterliao@gmail.com>
2018-02-13 16:35:24 +0800

    setup controller action index, show, edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :show]
+
+  def index
+    @products = Product.all
+  end
+  
+  def show
+    
+  end
+
+  def edit
+
+  end
+  
+
+  private
+  
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
